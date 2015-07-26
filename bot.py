b228df4cdc169277f1364cc46b12432e741605ea
Giorgos Logiotatidis <seadog@sealabs.net>
2015-07-26 18:19:55 +0300

    Fix TotalCommand default message.

--- bot.py
+++ bot.py
@@ -120,24 +120,24 @@ class TotalCommand(BotCommand):
     def __init__(self, *args, **kwargs):
         self.commands = [
              {'text': 'Day Total',
-                        'function': self.get_day_total},
+              'function': self.get_day_total},
              {'text': 'Week Total',
-                         'function': self.get_week_total},
+              'function': self.get_week_total},
              {'text': 'Month Total',
-                          'function': self.get_month_total},
+              'function': self.get_month_total},
              {'text': 'Year Total',
-                         'function': self.get_year_total},
+              'function': self.get_year_total},
              {'text': 'Grand Total',
-                          'function': self.get_grand_total}
+              'function': self.get_grand_total}
         ]
         super(TotalCommand, self).__init__(*args, **kwargs)
 
     def default(self, message):
-        msg = self._say(message, 'Which total?', reply_markup=telegram.ForceReply(selective=True))
+        text = '\n'.join(['({}) for {}'.format(i+1, cmd['text']) for i, cmd in enumerate(self.commands)])
+        msg = self._say(message, text, reply_markup=telegram.ForceReply(selective=True))
         self.queue(message.chat.id, msg.message_id, {'call': self.process_which_total})
 
     def process_which_total(self, message):
-
         if not message.text:
             self._say(message, 'Nope')
             return
@@ -160,7 +160,7 @@ class TotalCommand(BotCommand):
 
     def get_grand_total(self, message):
         tab = self.get_tab(message.chat.id)
-        self._say(message, 'Total: {}'.format(tab.grandtotal))
+        self._say(message, 'Grand Total: {}'.format(tab.grandtotal))
 
     @classmethod
     def match(cls, message):
