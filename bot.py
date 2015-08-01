7ff1680869da714b19f44580641f2a5248647b30
Giorgos Logiotatidis <seadog@sealabs.net>
2015-08-01 21:39:00 +0300

    Fix unicode in /export.

--- bot.py
+++ bot.py
@@ -337,21 +337,23 @@ class ExportCommand(BotCommand):
         import cStringIO
         import csv
         import os
-        from tempfile import NamedTemporaryFile
+        import codecs
+        from tempfile import mkstemp
 
         tab = self.get_tab(message.chat.id)
-        csvfile = NamedTemporaryFile(suffix='.csv', prefix='verese-export-', delete=False)
 
-        spamwriter = csv.writer(csvfile, delimiter=',')
-        spamwriter.writerow(['Person', 'Amount', 'Date', 'Reason'])
-        for entry in tab.entries:
-            user = self._db.root.users[entry.user_id]
-            user_repr = '{} {}'.format(user.first_name, user.last_name)
-            spamwriter.writerow([user_repr, entry.amount, entry.date, entry.reason])
-        csvfile.close()
+        csvhandle, csvfilename = mkstemp(suffix='.csv', prefix='verese-export-')
+        with os.fdopen(csvhandle, 'wb') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',')
+            csvwriter.writerow(['Person', 'Amount', 'Date', 'Reason'])
+            for entry in tab.entries:
+                user = self._db.root.users[entry.user_id]
+                user_repr = '{} {}'.format(user.first_name, user.last_name).strip()
+                row = [u'{}'.format(x).encode('utf-8') for x in [user_repr, entry.amount, entry.date, entry.reason]]
+                csvwriter.writerow(row)
 
-        self.bot._bot.sendDocument(chat_id=message.chat.id, document=open(csvfile.name, 'rb'))
-        os.unlink(csvfile.name)
+        self.bot._bot.sendDocument(chat_id=message.chat.id, document=open(csvfilename, 'rb'))
+        os.unlink(csvfilename)
 
 
 class VereseBot(object):
