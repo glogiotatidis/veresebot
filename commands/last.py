4cd701485646a812de98ca42785869d1c9130157
Giorgos Logiotatidis <seadog@sealabs.net>
2015-10-11 23:40:35 +0300

    Fixed length numbers.

--- commands/last.py
+++ commands/last.py
@@ -11,7 +11,7 @@ class LastCommand(BotCommand):
         howmany = int(match.groupdict(5)['howmany'])
         tab = self._db.get_or_create_tab(message.chat.id)[0]
         last_entries = u'\n'.join([
-            u'*{:.2f}* {} for {}'.format(entry.amount, entry.date.humanize(), entry.reason)
+            u'*{:7.2f}* {} for {}'.format(entry.amount, entry.date.humanize(), entry.reason)
             for entry in tab.entries[:howmany]])
         if not last_entries:
             last_entries = 'No entries!'
